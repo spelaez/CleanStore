cb00b301be86cad3a82dcc77edef860c0776d998
Santiago Pelaez <santi_rua@hotmail.com>
2018-05-12 19:46:42 -0500

    Initial Commit

---
+++ CleanStore/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CleanStore
+//
+//  Created by Santiago Peláez on 5/12/18.
+//  Copyright © 2018 Santiago Peláez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
